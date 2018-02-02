7e79021b0723794d3640894dda3d42378f0073e4
Vito <LinWei.Chen@acer.com>
2018-02-02 17:24:32 +0800

    modify no event description

--- web/event/message_event/no_event.php
+++ web/event/message_event/no_event.php
@@ -1,7 +1,7 @@
 ﻿ 
 <?php
 
-			$textMessage = emoji(100010).' 抱歉，小BLAH不了解您說什麼，沒辦法回答您。';
+			$textMessage = emoji(100010).' 抱歉，小Pawbo不了解您說什麼，沒辦法回答您。';
  			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textMessage);  
   
 
