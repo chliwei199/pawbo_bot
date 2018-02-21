39b5c10f63aa384ca166e061fe10dbfcdc3a5133
Vito <LinWei.Chen@acer.com>
2018-02-22 00:42:42 +0800

    add bot_postback_event

--- web/event/postback_event/bot_postback_event.php
+++ web/event/postback_event/bot_postback_event.php
@@ -1,53 +1,29 @@
 <?php
+		use LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 			/*
 			post event
 			*/
-			
+			$UtilityHandler= new App\event\UtilityHandler;				//create UtilityHandler object
+			$jsonString=$UtilityHandler->getJsonString();				//get json string from description
 			$getText = $event->getPostbackData();
 			
 			if ($getText) {
 				parse_str($getText, $data);
 				if (isset($data["map_key"])) {
-					$page = $data["map_key"];
-					$Text=str_replace(array('找菜：', '找酒：'), '', $page);
-					$pieces = explode("#", $Text); //[0]=sheet gid;[1]=page number 
-					switch ($page) {
-						// case 'Y':
-						// include('event/message_event/bot_map_search.php');
-						// break;
-						case 'Y':
-						include('event/message_event/bot_imagemap.php');
-						break;
-						default:
-							//include('event/message_event/bot_map_search.php');
-							$UtilityHandler= new App\event\UtilityHandler;				//create UtilityHandler object
-							$MultiMessageBuilder=$UtilityHandler->displayMoreItems($pieces[0],$pieces[1]);				//get json string from description
-							$bot->replyMessage($reply_token, $MultiMessageBuilder);
-						break;				
-					}
-
-				}
-				if (isset($data["map_cat"])) {
 					
-					$page = $data["map_cat"];
+					$page = $data["map_key"];
 
 					switch ($page) {
 						case 'Y':
-						include('event/message_event/bot_map_search.php');
+							$bot->pushMessage($user_id, new TextMessageBuilder($jsonString['bot_postback_event_continue_search'])); //message push
+							$getText="{\"FAQ:".$redis->checkStatus($user_id)."\"}";
+							include('event/message_event/bot_faq_ask.php');
 						break;
 						case 'N':
-						include('event/message_event/bot_imagemap.php');
+							$bot->pushMessage($user_id, new TextMessageBuilder($jsonString['bot_postback_event_pawbo_service'])); //message push
+							include('event/message_event/bot_menu_faq.php');
 						break;				
 					}
-
-				}
-
-				if (isset($data["map_cat_name"])) {
- 
-					$Text= $data["map_cat_name"];
-					include('event/message_event/bot_category.php');
-
-				}				
+				}					
 			}
-			
 ?>			
